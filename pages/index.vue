f969a2f718aff76654319cabc8a55fdec7479b49
Marcos Cantarutti <marcossandim21@gmail.com>
2024-06-25 20:31:27 -0300

    ajuste mobile
    '

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <header
     class="min-h-screen flex flex-col items-center gap-2 justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8"
   >
-    <span class="font-bold"
+    <span class="font-bold text-center"
       >LP DESENVOLVIMENTO - SISTEMA DE GERENCIAMENTO DE VAGAS</span
     >
     <Button label="Realizar login" icon="pi pi-user" @click="handleLogin" />
@@ -10,7 +10,7 @@
 </template>
 
 <script setup>
-import HomeLogin from '~/modules/HomeLogin/HomeLogin.vue';
+// import HomeLogin from '~/modules/HomeLogin/HomeLogin.vue';
 const router = useRouter();
 
 const handleLogin = () => {
